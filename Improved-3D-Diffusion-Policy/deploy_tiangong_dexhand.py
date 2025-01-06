226f9f6951b3e3d69b33500d81c7d5d7d5520e15
Vibrant <1262750373@qq.com>
2025-01-06 18:50:39 +0800

    add tiangong deploy draft

---
+++ Improved-3D-Diffusion-Policy/deploy_tiangong_dexhand.py
@@ -0,0 +1,122 @@
+import sys
+import os
+# use line-buffering for both stdout and stderr
+sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
+sys.stderr = open(sys.stderr.fileno(), mode='w', buffering=1)
+
+# Add the project directory to the Python path
+project_path = str(pathlib.Path(__file__).parent.parent)
+sys.path.append(project_path)
+
+import hydra
+import time
+from omegaconf import OmegaConf
+import pathlib
+from diffusion_policy_3d.workspace.base_workspace import BaseWorkspace
+from diffusion_policy_3d.robot_env.tiangong import TiangongDexEnvInference
+import diffusion_policy_3d.common.gr1_action_util as action_util
+import diffusion_policy_3d.common.rotation_util as rotation_util
+import tqdm
+import torch
+import os 
+os.environ['WANDB_SILENT'] = "True"
+# allows arbitrary python code execution in configs using the ${eval:''} resolver
+OmegaConf.register_new_resolver("eval", eval, replace=True)
+
+
+from diffusion_policy_3d.common.multi_realsense import MultiRealSense
+
+
+import numpy as np
+import torch
+from termcolor import cprint
+
+@hydra.main(
+    config_path=str(pathlib.Path(__file__).parent.joinpath(
+        'diffusion_policy_3d','config'))
+)
+
+def main(cfg: OmegaConf):
+    torch.manual_seed(42)
+    # resolve immediately so all the ${now:} resolvers
+    # will use the same time.
+    OmegaConf.resolve(cfg)
+    cls = hydra.utils.get_class(cfg._target_)
+    workspace: BaseWorkspace = cls(cfg)
+
+    if workspace.__class__.__name__ == 'DPWorkspace':
+        use_image = True
+        use_point_cloud = False
+    else:
+        use_image = False
+        use_point_cloud = True
+        
+    # fetch policy model
+    policy = workspace.get_model()
+    action_horizon = policy.horizon - policy.n_obs_steps + 1
+
+    # pour
+    roll_out_length_dict = {
+        "pour": 300,
+        "grasp": 1000,
+        "wipe": 300,
+    }
+    # task = "wipe"
+    task = "grasp"
+    # task = "pour"
+    roll_out_length = roll_out_length_dict[task]
+    
+    img_size = 84
+    num_points = 1024
+    first_init = True
+    record_data = True
+
+    env = TiangongDexEnvInference(obs_horizon=2, action_horizon=action_horizon, device="cpu",
+                                use_point_cloud=use_point_cloud,
+                                use_image=use_image,
+                                img_size=img_size,
+                                num_points=num_points)
+
+    
+    obs_dict = env.reset(first_init=first_init)
+
+    step_count = 0
+    
+    while step_count < roll_out_length:
+        with torch.no_grad():
+            action = policy(obs_dict)[0]
+            action_list = [act.numpy() for act in action]
+        
+        obs_dict = env.step(action_list)
+        step_count += action_horizon
+        print(f"step: {step_count}")
+
+    if record_data:
+        import h5py
+        root_dir = "/home/gr1p24ap0049/projects/gr1-learning-real/"
+        save_dir = root_dir + "deploy_dir"
+        os.makedirs(save_dir, exist_ok=True)
+        
+        record_file_name = f"{save_dir}/demo.h5"
+        color_array = np.array(env.color_array)
+        depth_array = np.array(env.depth_array)
+        cloud_array = np.array(env.cloud_array)
+        qpos_array = np.array(env.qpos_array)
+        with h5py.File(record_file_name, "w") as f:
+            f.create_dataset("color", data=np.array(color_array))
+            f.create_dataset("depth", data=np.array(depth_array))
+            f.create_dataset("cloud", data=np.array(cloud_array))
+            f.create_dataset("qpos", data=np.array(qpos_array))
+        
+        choice = input("whether to rename: y/n")
+        if choice == "y":
+            renamed = input("file rename:")
+            os.rename(src=record_file_name, dst=record_file_name.replace("demo.h5", renamed+'.h5'))
+            new_name = record_file_name.replace("demo.h5", renamed+'.h5')
+            cprint(f"save data at step: {roll_out_length} in {new_name}", "yellow")
+        else:
+            cprint(f"save data at step: {roll_out_length} in {record_file_name}", "yellow")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Improved-3D-Diffusion-Policy/diffusion_policy_3d/robot_env/tiangong.py
@@ -0,0 +1,132 @@
+import sys
+# use line-buffering for both stdout and stderr
+sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
+sys.stderr = open(sys.stderr.fileno(), mode='w', buffering=1)
+
+# Add the project directory to the Python path
+project_path = str(pathlib.Path(__file__).parent.parent)
+sys.path.append(project_path)
+
+import hydra
+import time
+from omegaconf import OmegaConf
+import pathlib
+from diffusion_policy_3d.workspace.base_workspace import BaseWorkspace
+import diffusion_policy_3d.common.gr1_action_util as action_util
+import diffusion_policy_3d.common.rotation_util as rotation_util
+import tqdm
+import torch
+import os 
+import cv2
+
+
+class TiangongDexEnvInference:
+    """
+    The deployment is running on the local computer of the robot.
+    """
+    def __init__(self, obs_horizon=2, action_horizon=8, device="gpu",
+                use_point_cloud=True, use_image=True, img_size=224,
+                 num_points=4096):
+        
+        # obs/action
+        self.use_point_cloud = use_point_cloud
+        self.use_image = use_image
+
+        # horizon
+        self.obs_horizon = obs_horizon
+        self.action_horizon = action_horizon
+        
+        # init robot env
+        from inrocs.robot_env.tianyi_env import tianyi_env as robot_env
+        robot_env.reset_to_parepre()
+    
+    def step(self, action_list):
+        
+        for action_id in range(self.action_horizon):
+            act = action_list[action_id]
+            self.action_array.append(act)
+            
+            # de normalize action
+            action_pred = self.process_action(act)
+            
+            robot_env.stepfull(action_pred)
+
+            obs = robot_env.get_obs_full()
+            
+            self.cloud_array.append(self.process_cloud(obs))
+            self.color_array.append(self.process_color(obs))
+            self.depth_array.append(self.process_depth(obs))
+            self.env_qpos_array.append(self.process_qpos(obs))
+            time.sleep(0.2)
+        
+        agent_pos = np.stack(self.env_qpos_array[-self.obs_horizon:], axis=0)
+    
+        obs_cloud = np.stack(self.cloud_array[-self.obs_horizon:], axis=0)
+        obs_img = np.stack(self.color_array[-self.obs_horizon:], axis=0)
+            
+        obs_dict = {
+            'agent_pos': torch.from_numpy(agent_pos).unsqueeze(0).to(self.device),
+        }
+        if self.use_point_cloud:
+            obs_dict['point_cloud'] = torch.from_numpy(obs_cloud).unsqueeze(0).to(self.device)
+        if self.use_image:
+            obs_dict['image'] = torch.from_numpy(obs_img).permute(0, 3, 1, 2).unsqueeze(0)
+
+        return obs_dict
+    
+    def reset(self, first_init=True):
+        robot_env.reset_to_parepre()
+        # warm up
+        import time
+        time.sleep(2)
+        obs = robot_env.get_obs_full()
+        agent_pos = self.process_qpos(obs)
+        obs_cloud = self.process_cloud(obs)
+        obs_img = self.process_color(obs)
+        obs_depth = self.process_depth(obs)
+        obs_dict = {
+            'agent_pos': torch.from_numpy(agent_pos).unsqueeze(0).to(self.device),
+        }
+        if self.use_point_cloud:
+            obs_dict['point_cloud'] = torch.from_numpy(obs_cloud).unsqueeze(0).to(self.device)
+        if self.use_image:
+            obs_dict['image'] = torch.from_numpy(obs_img).permute(0, 3, 1, 2).unsqueeze(0)
+        return obs_dict
+
+    # de normalize action
+    def process_action(self, action):
+        # TODO: de normalize action
+        return action
+    
+    def process_cloud(self, obs):
+        return None
+    
+    def process_color(self, obs, show_img=True):
+        # (w, h) for cv2.resize
+        img_new_size = (84, 84) #(480, 640)
+        all_cam_images = []
+        for cam_name in self.args['camera_names']:
+            curr_image = obs['images'][cam_name]
+            curr_image = cv2.imdecode(curr_image, cv2.IMREAD_COLOR)
+            curr_image = cv2.resize(curr_image, dsize=img_new_size)
+            if show_img:
+                rgb_image = curr_image[:, :, ::-1]
+                cv2.imshow(f"{cam_name} image", rgb_image)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break  
+            all_cam_images.append(curr_image)
+        all_cam_images = (all_cam_images / 255.0).astype(np.float32)
+        return all_cam_images
+    
+    def process_depth(self, obs):
+        return None
+    
+    def process_qpos(self, obs):
+        qpos = obs['qpos']
+        left_jpos = qpos[:7]
+        left_hand_jpos = qpos[7:13]
+        right_jpos = qpos[13:20]
+        right_hand_jpos = qpos[20:26]
+        qpos = np.concatenate((left_hand_jpos, right_hand_jpos, left_jpos, right_jpos))
+        return qpos
+    
